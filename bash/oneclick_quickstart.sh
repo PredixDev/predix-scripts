cee33fb18e0c65050cb462c4966785561796ce7b
Predix Robot <predix-robot@ge.com>
2017-02-01 14:02:54 -0800

    First commit

---
+++ bash/oneclick_quickstart.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+set -e
+BRANCH="master"
+SKIP_SETUP=false
+QUICKSTART_ARGS=""
+
+while (( "$#" )); do
+  if [[ $1 == "--skip-setup" ]]; then
+    SKIP_SETUP=true
+    shift
+  elif [[ $1 == "-b" ]]; then
+    QUICKSTART_ARGS="$QUICKSTART_ARGS $1 $2 "
+    BRANCH=$2
+    shift
+    shift
+  else 
+    QUICKSTART_ARGS="$QUICKSTART_ARGS $1 "
+    shift
+  fi
+done
+
+echo "SKIP_SETUP            : $SKIP_SETUP"
+echo "BRANCH                : $BRANCH"
+echo "QUICKSTART_ARGS       : $QUICKSTART_ARGS"
+
+
+SETUP_MAC="https://github.build.ge.com/adoption/local-setup/$BRANCH/setup-mac.sh"
+PREDIX_SCRIPT_DIR_NAME=predix-scripts
+PREDIX_SCRIPT_REPO=https://github.build.ge.com/adoption/$PREDIX_SCRIPT_DIR_NAME.git
+
+function check_internet() {
+  set +e
+  echo ""
+  echo "Checking internet connection..."
+  curl "http://google.com" > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "Unable to connect to internet, make sure you are connected to a network and check your proxy settings if behind a corporate proxy"
+    echo "If you are behind a corporate proxy, set the 'http_proxy' and 'https_proxy' environment variables."
+    exit 1
+  fi
+  echo "OK"
+  echo ""
+  set -e
+}
+
+function run_setup() {
+  check_internet
+  bash <(curl -s -L "$SETUP_MAC") --git --cf --nodejs --maven
+}
+
+function git_clone_repo() {
+  if [ -d predix-scripts ]; then
+    echo "The predix-scripts dir already exists."
+    read -p "Should we delete it and proceed?> " -t 30 answer
+    verifyAnswer answer
+    echo ""
+    rm -rf predix-scripts
+  fi
+  echo ""
+  echo "Cloning predix script repo ..."
+  git clone $PREDIX_SCRIPT_REPO
+}
+
+function verifyAnswer() {
+  if [[ -z $answer ]]; then
+    echo -n "Specify (yes/no)> "
+    read answer
+  fi
+  if [[ ${answer:0:1} == "y" ]] || [[ ${answer:0:1} == "Y" ]]; then
+    return
+  fi
+  exit 1
+}
+
+if $SKIP_SETUP; then
+  check_internet
+else
+    echo "Welcome to the Predix Build An Application Quick Start."
+    run_setup
+    echo ""
+    echo "The required tools have been installed. Proceed with the setting up services and application."
+    echo ""
+    echo ""
+fi
+
+git_clone_repo
+$PREDIX_SCRIPT_DIR_NAME/bash/quickstart.sh $QUICKSTART_ARGS
---
+++ bash/scripts/buildMavenBundle.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+buildMavenBundleRootDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+buildMavenBundleLogDir="$buildMavenBundleRootDir/../log"
+
+source "$buildMavenBundleRootDir/predix_funcs.sh"
+source "$buildMavenBundleRootDir/variables.sh"
+source "$buildMavenBundleRootDir/error_handling_funcs.sh"
+source "$buildMavenBundleRootDir/files_helper_funcs.sh"
+source "$buildMavenBundleRootDir/curl_helper_funcs.sh"
+MACHINE_HOME="$1"
+CURRENT_DIR=$(pwd)
+echo "CURRENT_DIR : $CURRENT_DIR"
+cd ../..
+if [[ $RUN_COMPILE_REPO -eq 1 ]]; then
+  mvn -q clean install -U -Dmaven.compiler.source=1.8 -Dmaven.compiler.target=1.8 -f pom.xml -s $MAVEN_SETTNGS_FILE
+else
+  mvn clean dependency:copy -Dmdep.useBaseVersion=true -s $MAVEN_SETTNGS_FILE
+fi
+
+mvn help:active-profiles
+
+PROJECT_ARTIFACT_ID=$(mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.artifactId | grep -e '^[^\[]')
+PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep -e '^[^\[]')
+MACHINE_BUNDLE="$PROJECT_ARTIFACT_ID-$PROJECT_VERSION.jar"
+echo "MACHINE_BUNDLE_JAR : $MACHINE_BUNDLE"
+
+#SOLUTION_INI="sed -i 's#{MACHINE_BUNDLE_JAR}#${MACHINE_BUNDLE}#' config/solution.ini > $MACHINE_HOME/machine/bin/vms/solution.ini"
+#echo "SOLUTION_INI : $SOLUTION_INI"
+#sed "s\#MACHINE_BUNDLE_JAR\#${MACHINE_BUNDLE}\#" config/solution.ini > "$MACHINE_HOME/machine/bin/vms/solution.ini"
+#__echo_run "sed \"s#MACHINE_BUNDLE_JAR#$MACHINE_BUNDLE#\" \"config/solution.ini\" > $MACHINE_HOME/machine/bin/vms/solution.ini"
+__find_and_replace_string "{MACHINE_BUNDLE_VERSION}" "$PROJECT_VERSION" "config/solution.ini" "$buildBasicAppLogDir" "$MACHINE_HOME/machine/bin/vms/solution.ini"
+
+#sed 's#{MACHINE_BUNDLE_JAR}#${MACHINE_BUNDLE}#' config/solution.ini > "$MACHINE_HOME/machine/bin/vms/solution.ini"
+#sed -i -e "s#<name>{MACHINE_BUNDLE_JAR}</name>#<name>$MACHINE_BUNDLE</name>#" config/solution.ini
+#__echo_run cp config/solution.ini $MACHINE_HOME/machine/bin/vms/solution.ini
+__echo_run cp target/$MACHINE_BUNDLE "$MACHINE_HOME/machine/bundles"
+
+echo "#################### Build and setup the adatper end ####################"
+
+cd "$CURRENT_DIR"
+if [[ $SKIP_SERVICES -eq 0 ]]; then
+	__print_center "Archive and copy Predix Machine container to the target device" "#"
+ 	__echo_run ./scripts/predix_machine_setup.sh "$TEMP_APP" "0" "1"
+else
+	read -p "Enter the IP Address of your device(press enter if you want to copy to different directory on the host)> " TARGETDEVICEIP
+	TARGETDEVICEIP=${TARGETDEVICEIP:localhost}
+	read -p "Enter the Username on your device(this is the username which you use to ssh to the device> " TARGETDEVICEUSER
+	read -p "Enter the Predix Machine Home Directry> " PREDIX_MACHINE_ROOT_DIR
+
+	__echo_run cp "$CURRENT_DIR/../../target/$MACHINE_BUNDLE" "$MACHINE_HOME/machine/bundles"
+
+	__echo_run scp "$CURRENT_DIR/../../target/$MACHINE_BUNDLE" $TARGETDEVICEUSER@$TARGETDEVICEIP:$PREDIX_MACHINE_ROOT_DIR/machine/bundles
+	echo "Transferred Predix Machine bundle!" "#"
+fi
