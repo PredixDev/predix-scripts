4e544b6b1445a71c659fc0501d1b72b1e50614c3
turnerth <turnerth@ge.com>
2017-08-03 14:51:21 -0700

    Update my-routes.sh

--- bash/common/my-routes.sh
+++ bash/common/my-routes.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 for ROUTE in `cf curl "/v2/routes" | jq -r ".resources[].metadata.guid"`; do
+  HOST=$(cf curl "/v2/routes/$ROUTE" | jq -r ".entity.host")
   SPACE_GUID=$(cf curl "/v2/routes/$ROUTE" | jq -r ".entity.space_guid")
-
   SPACE_INFO=$(cf curl "/v2/spaces/$SPACE_GUID")
   SPACE_NAME=$(echo $SPACE_INFO | jq -r ".entity.name")
   ORG_GUID=$(echo $SPACE_INFO | jq -r ".entity.organization_guid")
   ORG_NAME=$(cf curl /v2/organizations/$ORG_GUID | jq -r ".entity.name")
 
-  echo "Route:" $ROUTE "Org Name: " $ORG_NAME "Space Name: " $SPACE_NAME
+  echo "Host:" $HOST "Org Name: " $ORG_NAME "Space Name: " $SPACE_NAME
 done
