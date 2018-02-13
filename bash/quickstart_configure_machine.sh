3b5c2b1f0499251b424956cbc35077a8c3d58307
susheelvc <susheel.choudhari@ge.com>
2018-02-13 12:15:19 -0800

    Update quickstart_configure_machine.sh

--- bash/quickstart_configure_machine.sh
+++ bash/quickstart_configure_machine.sh
@@ -175,8 +175,9 @@ if [[ "$UPGRAGE_MACHINE" == "1" ]]; then
 	systemctl stop predixmachine
 	curl -O $PREDIX_MACHINE_URL
 	mkdir -p "$PREDIX_MACHINE_HOME-$CURRENT_MACHINE_VERSION"
-	cp -rf  $PREDIX_MACHINE_HOME "$PREDIX_MACHINE_HOME-$CURRENT_MACHINE_VERSION"
-	find . ! -name '$0' -type f -exec rm -f {} +
+	cp -rf $PREDIX_MACHINE_HOME "$PREDIX_MACHINE_HOME-$CURRENT_MACHINE_VERSION"
+	rm -rf $PREDIX_MACHINE_HOME/*
+	#find . ! -name '$0' -type f -exec rm -f {} +
 	#mkdir -p $PREDIX_MACHINE_HOME
 	tar xvf $PREDIXMACHINE_TAR_FILENAME -C $PREDIX_MACHINE_HOME
 	cp $PREDIX_MACHINE_HOME-17.1.2/machine/bin/predix/setvars.sh $PREDIX_MACHINE_HOME/machine/bin/predix/setvars.sh
