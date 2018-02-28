a4014245089a5a59253c90eb086aeaaeb82470df
tom <502588299@ge.com>
2018-02-28 17:32:06 +0000

    Release 1.1.86

--- bash/scripts/edge-starter-kit-service.sh
+++ bash/scripts/edge-starter-kit-service.sh
@@ -78,9 +78,9 @@ function edge-starter-kit-service-main() {
     fi
     __append_new_head_log "Retrieving the application $KIT_SERVICE_APP_NAME" "-" "$logDir"
     if [[ $RUN_COMPILE_REPO -eq 1 ]]; then
-      mvn clean package -U -s $MAVEN_SETTINGS_FILE
+      mvn clean package -U -B -s $MAVEN_SETTINGS_FILE
     else
-      mvn clean dependency:copy -s $MAVEN_SETTINGS_FILE
+      mvn clean dependency:copy -B -s $MAVEN_SETTINGS_FILE
     fi
     __append_new_head_log "Deploying the application $KIT_SERVICE_APP_NAME" "-" "$logDir"
 
